16cfeed2b5eef4f8246315e60d65608e478950ed
Joshua  Zdanowicz <joshuazdanowicz@yahoo.com>
2019-02-09 19:41:17 -0500

    Update Lab03

--- Lab03JZdanowicz.playground/Contents.swift
+++ Lab03JZdanowicz.playground/Contents.swift
@@ -20,62 +20,165 @@ var pi3: Double = 22/7 // Pi shows as 3.142857142857143
  Double is accurate to as many as 15 decimal digits, in most
  cases, where memory size doesn't matter Double is prefered.*/
 
+print("Pi As:","Int",pi1,"Float", pi2,"Double", pi3)
+//                                                                                                                     //
+
+var str = "Hello, Josh"
+
 
 class Counter {
+    
     var count = 0
+    
     let maxCount = 10
-
+    
+    
+    
     func increment() {
-       count += 1
+        
+        count += 1
         
     }
-//parameter with argument label and parameter name
     
-    func increment(by amount: Int) {
-        count += amount }
-    func reset() { count = 0
+    //parameter with argument label and parameter name
+    
+    // Replace by amount with byAmount. You can't have spaces in identifiers (parameters, variables, etc.)
+    
+    func increment(byAmount: Int) {
+        
+        count += byAmount
+        
+        // needed to close the method body
+        
+    }
+    
+    
+    func reset() {
+        
+        count = 0
         
     }
     
     func decrement() {
         
         count -= 1
-        if count > 10 {
-            maxCount
-        }
+        
+        
+        
+        //commenting again
+        
+        //if count > 10 {
+        
+        //    maxCount
+        
+        //}
+        
     }
     
     func decrement(byAmount:  Int ) {
-    for _ in 1...byAmount {
+        
+        for _ in 1...byAmount {
+            
             decrement()
+            
         }
-        if count > 10 {
-            maxCount
-        }
+        
+        // not sure what this is doing.
+        
+        //if count > 10 {
+        
+        //    maxCount
+        
+        //}
+        
+        
+        
     }
     
-    func getCount() {
-            self
-        if count > 10 {
-            maxCount
-        }
-            
-            
-        }
+    
+    
+    // Needed to add -> Int to indicate that the method is returning a value
+    
+    func getCount() -> Int {
+        
+        // not sure what you going for with self by itself? We can use self to prefix properties.
+        
+        // read https://docs.swift.org/swift-book/LanguageGuide/Methods.html for more info
+        
+        //self
+        
+        //if count > 10 {
+        
+        //    maxCount
+        
+        //}
+        
+        return self.count
+        
+    }
     func display() {
-            print("count")
-            print(getCount())
         
-        if count > 10 {
-            maxCount
-        }
-        }
-    
-       
+        // This is displaying a string literal with the value count
+        
+        //print("count")
+        
+        // You should print the actual value of count:
+        
+        //print(count) getCount() on the following line works as well
+        
+        print(getCount())
+        
+        
+        
+        // commented again
+        
+        //if count > 10 {
+        
+        //    maxCount
+        
+        //}
+        
+        
         
     }
+    
+}
+var counter = Counter()
+
+counter.count = 10
 
 
+counter.decrement()
 
+counter.decrement()
+
+counter.display()
+
+
+counter.decrement(byAmount: 5)
+
+counter.display()
+
+
+counter.increment(byAmount: 20)
+
+counter.display()
+
+//                                                                                                                     //
+
+func combineAll(numbers: [Int], starter: String) -> String {
+    var numbersCombined = starter + ": "
+    for number in numbers {
+        numbersCombined = numbersCombined + String(number) + " " }
+    return numbersCombined }
+print(combineAll(numbers: [9, 8, 7], starter: "Combined"))
+
+func concatAll(names: [String], starter: String) -> String {
+    var namesCombined = starter + ": "
+    for name in names {
+        namesCombined = namesCombined + String(name) + " " }
+    return namesCombined }
+print(concatAll(names: ["Jim", "Matt", "Josh", "Jess"], starter: "Combined"))
+    
 
 
