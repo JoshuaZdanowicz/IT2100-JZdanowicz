276658281b5da59db50e628b21915d98135085d0
Joshua  Zdanowicz <joshuazdanowicz@yahoo.com>
2019-02-19 21:49:23 -0500

    Update Lab05

--- Lab05Calculator/Lab05 Calculator/ViewController.swift
+++ Lab05Calculator/Lab05 Calculator/ViewController.swift
@@ -2,14 +2,21 @@
 //  ViewController.swift
 //  Lab05Calculator
 //
-//  Created by Joshua Zdanowicz on 2/19/19.
+//  Created by Joshua Zdanowicz on 2/13/19.
 //  Copyright © 2019 Joshua Zdanowicz. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var fullName: UILabel!
+    @IBOutlet weak var inputFirst: UITextField!
+    @IBOutlet weak var inputLast: UITextField!
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
