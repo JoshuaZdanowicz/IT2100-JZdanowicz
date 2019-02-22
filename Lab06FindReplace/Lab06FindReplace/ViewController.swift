d7fc84e2276dad916a8251d655148e7c12fdaa04
Joshua  Zdanowicz <joshuazdanowicz@yahoo.com>
2019-02-21 19:05:33 -0500

    Update Lab06  w/ outlets and actions for storyboard

--- Lab06FindReplace/Lab06FindReplace/ViewController.swift
+++ Lab06FindReplace/Lab06FindReplace/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var FindInput: UITextField!
+    @IBOutlet weak var ReplaceInput: UITextField!
+    @IBOutlet weak var TextInput: UITextView!
+    
+    
+    @IBOutlet weak var UpdateText: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
