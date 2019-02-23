393261215e39406dfe84804525fbe619514f3e6b
JoshuaZdanowicz <34405720+JoshuaZdanowicz@users.noreply.github.com>
2019-02-23 17:34:31 -0500

    Updated Comment

--- Lab06FindReplace/Lab06FindReplace/ViewController.swift
+++ Lab06FindReplace/Lab06FindReplace/ViewController.swift
@@ -5,7 +5,8 @@
 //  Created by Joshua Zdanowicz on 2/19/19.
 //  Copyright © 2019 Joshua Zdanowicz. All rights reserved.
 
-/*I'm not sure what to do to get this app to run properly, first in terms of my logic which I'm sure is wrong/incomplete and the breakpoint at line 17. Not even sure if my connections to the storyboard are correct.*/
+/*I'm not sure what to do to get this app to run properly, first in terms of my logic which I'm sure is wrong/incomplete and the 
+breakpoint at line 17. Not even sure if my connections to the storyboard are correct.*/
 
  
  
