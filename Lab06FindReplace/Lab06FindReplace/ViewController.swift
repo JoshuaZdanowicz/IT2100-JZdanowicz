cf1d1c1661edfce0b2ee5b5c28e5088e68839a36
Joshua  Zdanowicz <joshuazdanowicz@yahoo.com>
2019-02-23 17:21:33 -0500

    Added Comments

--- Lab06FindReplace/Lab06FindReplace/ViewController.swift
+++ Lab06FindReplace/Lab06FindReplace/ViewController.swift
@@ -4,14 +4,17 @@
 //
 //  Created by Joshua Zdanowicz on 2/19/19.
 //  Copyright © 2019 Joshua Zdanowicz. All rights reserved.
-//
 
+/*I'm not sure what to do to get this app to run properly, first in terms of my logic which I'm sure is wrong/incomplete and the breakpoint at line 17. Not even sure if my connections to the storyboard are correct.*/
+
+ 
+ 
 import UIKit
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var FindInput: UITextField!
-    @IBOutlet weak var ReplaceInput: UITextField!
+    @IBOutlet weak var ReplaceInput: UITextField! //FIX ME:Thread 1: breakpoint 1.4
     @IBOutlet weak var TextInput: UITextView!
     
     
@@ -34,4 +37,4 @@ class ViewController: UIViewController {
 
 
 }
-
+ 
