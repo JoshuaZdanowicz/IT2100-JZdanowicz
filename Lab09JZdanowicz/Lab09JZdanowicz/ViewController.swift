7e7a3ddab690bf98725a1e986a7996a307b76e0f
Joshua  Zdanowicz <joshuazdanowicz@yahoo.com>
2019-03-24 14:36:41 -0400

    Updated Lab 09

--- Lab09JZdanowicz/Lab09JZdanowicz/ViewController.swift
+++ Lab09JZdanowicz/Lab09JZdanowicz/ViewController.swift
@@ -9,12 +9,48 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var userOutput: UILabel!
+    @IBOutlet weak var userName: UILabel!
+    @IBOutlet weak var userEmail: UILabel!
+    @IBOutlet weak var favoriteLanguage: UILabel!
+    @IBOutlet weak var selectedAction:UITextView!
+    
+    var nameStr: String?
+    var emailStr: String?
+    var nameTextField: UITextField?
+    var emailTextField: UITextField?
+
+    
+    
+    
+    @IBAction func showAltertWithFields(_ sender: Any) {
+        
+        let alert = UIAlertController(title: "Enter Name and Email", message: "name and email address required.", preferredStyle: .alert)
+        
+        present(alert, animated: true, completion: nil)
+    }
+        
+     @IBAction func showAlertWithOptions(_ sender: Any) {
+        
+        let alert = UIAlertController(title: "Select Language  ", message: "Please pick a programming language .",
+            preferredStyle: .alert)
+
+        present(alert, animated: true, completion: nil)
+        }
+
+    @IBAction func showActionSheet(_ sender: Any) {
+        
+        let actionsheet = UIAlertController(title: "Form Complete.", message: "Do you want to send this info?", preferredStyle: .actionSheet)
+        
+                present(actionsheet, animated: true, completion: nil)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    
+    
 }
 
