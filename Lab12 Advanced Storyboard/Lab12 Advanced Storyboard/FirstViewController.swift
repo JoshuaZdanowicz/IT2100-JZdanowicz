15c5d0d4823aaeabbcbbc04d37583f79a1706152
Joshua Zdanowicz <joshuazdanowicz@yahoo.com>
2019-04-09 23:37:52 -0400

    Lab 12 storyboard finished

---
+++ Lab12 Advanced Storyboard/Lab12 Advanced Storyboard/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Lab12 Advanced Storyboard
+//
+//  Created by Joshua Zdanowicz on 4/9/19.
+//  Copyright © 2019 Joshua Zdanowicz. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Lab12 Advanced Storyboard/Lab12 Advanced Storyboard/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Lab12 Advanced Storyboard
+//
+//  Created by Joshua Zdanowicz on 4/9/19.
+//  Copyright © 2019 Joshua Zdanowicz. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
